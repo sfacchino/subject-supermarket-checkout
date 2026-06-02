caf64bcea650e578a33111505076de5a5d407dc5
sfacchino <sfacchino@edenschool.fr>
2026-06-02 17:26:39 +0200

    cm

--- js/basket.js
+++ js/basket.js
@@ -1,14 +1,10 @@
 const btnadd = document.querySelector(".positive");
 const btnremove = document.querySelector(".negative");
 const number = document.querySelector(".number");
-const div = document.querySelector(".crt");
 
 let nbr = 0;
 
-div.addEventListener("click", () => {
-    btnadd.style.display = ("block");
-    btnremove.style.display = ("block");
-})
+
 
 btnadd.addEventListener("click", () => {
     nbr++
